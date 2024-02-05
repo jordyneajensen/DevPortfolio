613e03977e3b352613d71a46452a0f8b7687a7d3
Jordyn <jordyneajensen@gmail.com>
2024-02-05 09:41:21 -0600

    restructuring by adding pages

---
+++ my-tailwind-app/pages/navbar.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import Link from "next/link";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+function navbar() {
+  return (
+    <div>navbar</div>
+  )
+}
+
+export default navbar
